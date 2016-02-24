cc21df62c5a73bd546765b5b0055265f24747ae6
unknown <alforeha@CT156-44.adit.mines.edu>
2016-02-23 19:30:13 -0700

    changed size

---
+++ MyDrawing.java
@@ -0,0 +1,38 @@
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+public class MyDrawing extends JPanel {
+	private Cat cat;
+	public MyDrawing() 
+	{
+		// Add critters to the drawing
+		cat = new Cat();
+	}
+	// paintComponent is called automatically when the frame needs
+	// to display (e.g., when the program starts)
+	public void paintComponent(Graphics g) {
+		cat.draw(g, 50, 50);
+	}
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		// Create a JPanel (MyDrawing), we'll draw on this
+		frame.setContentPane(new MyDrawing());
+		// Program will end when the window is closed
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// Set the size (otherwise only title bar displays)
+		// SIZE CHANGED TO 200
+		frame.setSize(200, 400);
+		//CHANGED THE SETSIZE
+		
+		// Frame will not display until you set visible true
+		frame.setVisible(true);
+	}
+
+}
